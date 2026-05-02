ab5e171041e9e359f16b7d9bf1ca40b63b216941
77tuo <164346270+77tuo@users.noreply.github.com>
2026-05-02 14:15:02 +0800

    Delete 1_sql/0_create_database.sql

--- 1_sql/0_create_database.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE IF NOT EXISTS olist;
-USE olist;
-
--- DROP DATABASE IF EXISTS olist;
-
--- 展示csv文件可安全导入的数据路径，将这里显示的路径粘贴到import_data.sql中指向的路径
-SHOW VARIABLES LIKE 'secure_file_priv';
-
